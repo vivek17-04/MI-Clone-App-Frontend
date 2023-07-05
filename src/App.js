e23a1d40bf9cbe8e8ae7cf3c0fa66b36d8863ab0
vivek <vkapashera77@gmail.com>
2023-07-05 15:51:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import PreNavbar from './components/PreNavbar';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Slider from './components/Slider';
+import data from '../src/data/data.json';
+import Offers from './components/Offers';
+import Heading from './components/Heading';
+import StarProduct from './components/StarProduct';
+import HotAccessoriesMenu from './components/HotAccessoriesMenu';
+import HotAccessories from './components/HotAccessories';
+import ProductReviews from './components/ProductReviews';
+import Videos from './components/Videos';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import NavOptios from './components/NavOptios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <PreNavbar />
+      <Navbar />
+      <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories} />
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCT" />
+      <StarProduct starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+      <Routes>
+        <Route exact path='/music'
+          element={<HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music} />
+          } />
+
+        <Route exact path='/smartDevice'
+          element={<HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
+
+        <Route exact path='/home'
+          element={<HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
+
+        <Route exact path='/lifestyle'
+          element={<HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle} />} />
+
+        <Route exact path='/mobileAccessories'
+          element={<HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
+
+      </Routes>
+      <Heading text="PRODUCT REVIEWS" />
+      <ProductReviews productReviews={data.productReviews} />
+      <Heading text='VIDEOS'/>
+      <Videos videos={data.videos}/>
+      <Heading text="IN THE PRESS" />
+      <Banner banner={data.banner}/>
+      <Footer footer={data.footer}/>
+    </BrowserRouter>
   );
 }
 
